34509c906c0e2b45c7de0d32a34c81a262283792
evanchristians <evanryk@gmail.com>
2024-11-12 23:47:57 +0200

    refactor: array of objects was not a good idea, i love map, i should make it my goto more often

--- main.js
+++ main.js
@@ -1,38 +1,102 @@
+const NUM_PARTICLES = 2**14;
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const WIDTH = (canvas.width = canvas.offsetWidth);
 const HEIGHT = (canvas.height = canvas.offsetHeight);
-const GRAIN_SIZE = 10;
 
-ctx.fillStyle = "#2F3337";
+const GRAIN_SIZE = Math.floor(Math.sqrt((WIDTH * HEIGHT) / NUM_PARTICLES));
 
-ctx.fillRect(0, 0, WIDTH, HEIGHT);
+const GRID_HEIGHT = Math.floor(HEIGHT / GRAIN_SIZE);
+const GRID_WIDTH = Math.floor(WIDTH / GRAIN_SIZE);
 
-let color = 0;
+const mapKey = (x, y) => `${x},${y}`;
 
-const speed = GRAIN_SIZE * 1;
-class Sandbox {
+class SandBox {
   constructor() {
-    this.sand = [];
+    this.sand = new Map();
+    this.color = 0;
     this.update = this.update.bind(this);
     this.render = this.render.bind(this);
-    3;
-    this.spamSand = false;
     this.init();
   }
 
-  init() {
-    window.requestAnimationFrame(this.render);
+  createSand(x, y, value = new SandParticle(this.color)) {
+    return this.sand.set(mapKey(x, y), value);
+  }
 
+  emptyCell(x, y) {
+    return this.sand.delete(mapKey(x, y));
+  }
+
+  getSand(x, y) {
+    return this.sand.get(mapKey(x, y));
+  }
+
+  hasSand(x, y) {
+    return this.sand.has(mapKey(x, y));
+  }
+
+  moveDown(x, y) {
+    this.createSand(x, y + 1, this.getSand(x, y));
+    this.emptyCell(x, y);
+  }
+
+  moveDownAndLeft(x, y) {
+    if (x - 1 < 0) {
+      return false;
+    }
+    if (this.hasSand(x - 1, y + 1)) {
+      return false;
+    }
+    this.createSand(x - 1, y + 1, this.getSand(x, y));
+    this.emptyCell(x, y);
+    return true;
+  }
+
+  moveDownAndRight(x, y) {
+    if (x >= GRID_WIDTH) {
+      return false;
+    }
+    if (this.hasSand(x + 1, y + 1)) {
+      return false;
+    }
+    this.createSand(x + 1, y + 1, this.getSand(x, y));
+    this.emptyCell(x, y);
+    return true;
+  }
+
+  drawBlockOfSand(x, y) {
+    for (let i = -2; i < 2; i++) {
+      for (let j = -2; j < 2; j++) {
+        if (this.hasSand(x + i, y + j)) {
+          continue;
+        }
+        this.createSand(x + i, y + j);
+      }
+    }
+  }
+
+  createEmptySpace() {
+    for (let x = 0; x < GRID_WIDTH; x++) {
+      for (let y = 0; y < GRID_HEIGHT; y++) {
+        this.emptyCell(x, y);
+      }
+    }
+  }
+
+  init() {
+    this.createEmptySpace();
+    window.requestAnimationFrame(this.render);
     setInterval(this.update, 1000 / 60);
 
     let drawing = false;
 
     canvas.addEventListener("mousedown", (e) => {
       this.drawBlockOfSand(
-        e.clientX - e.target.offsetLeft,
-        e.clientY - e.target.offsetTop
+        Math.floor((e.clientX - e.target.offsetLeft) / GRAIN_SIZE),
+        Math.floor((e.clientY - e.target.offsetTop) / GRAIN_SIZE)
       );
       drawing = true;
     });
@@ -44,44 +108,38 @@ class Sandbox {
     canvas.addEventListener("mousemove", (e) => {
       if (drawing) {
         this.drawBlockOfSand(
-          e.clientX - e.target.offsetLeft,
-          e.clientY - e.target.offsetTop
+          Math.floor((e.clientX - e.target.offsetLeft) / GRAIN_SIZE),
+          Math.floor((e.clientY - e.target.offsetTop) / GRAIN_SIZE)
         );
       }
     });
   }
 
-  drawBlockOfSand(x, y, size = 2) {
-    const { x: x1, y: y1 } = this.normalizePoint(x, y);
-    for (let i = -size / 2; i < size / 2; i++) {
-      for (let j = -size / 2; j < size / 2; j++) {
-        if (this.sand.some((grain) => grain.x == x1 + i * GRAIN_SIZE && grain.y == y1 + j * GRAIN_SIZE)) {
+  update() {
+    this.color += 0.2;
+    if (this.color > 360) {
+      this.color = 0;
+    }
+
+    for (let y = GRID_HEIGHT; y >= 0; y--) {
+      for (let x = GRID_WIDTH; x >= 0; x--) {
+        if (!this.hasSand(x, y)) {
           continue;
         }
-        this.sand.push(
-          new Sand(x1 + i * GRAIN_SIZE, y1 + j * GRAIN_SIZE, this)
-        );
+        if (y == GRID_HEIGHT) {
+          continue;
+        }
+        if (this.hasSand(x, y + 1)) {
+          const goLeftFirst = Math.random() > 0.5;
+          if (goLeftFirst) {
+            this.moveDownAndLeft(x, y) ?? this.moveDownAndRight(x, y);
+            continue;
+          }
+          this.moveDownAndRight(x, y) ?? this.moveDownAndLeft(x, y);
+          continue;
+        }
+        this.moveDown(x, y);
       }
-    }
-  }
-
-  normalizePoint(x, y) {
-    return {
-      x: Math.floor(x / GRAIN_SIZE) * GRAIN_SIZE,
-      y: Math.floor(y / GRAIN_SIZE) * GRAIN_SIZE,
-    };
-  }
-
-  update() {
-    if (this.spamSand) {
-      this.sand.push(new Sand(this.normalizePoint(Math.random() * WIDTH), 0));
-    }
-    for (const grain of this.sand) {
-      grain.update(this.sand);
-    }
-    color += 0.2;
-    if (color > 360) {
-      color = 0;
     }
   }
 
@@ -89,132 +147,28 @@ class Sandbox {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     ctx.fillStyle = "#2F3337";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
-    for (const grain of this.sand) {
-      grain.draw();
+
+    for (const [xy, grain] of this.sand.entries()) {
+      const [x, y] = xy.split(",").map(Number);
+      if (grain) {
+        grain.draw(x, y);
+      }
     }
 
     window.requestAnimationFrame(this.render);
   }
 }
 
-class Sand {
-  /**
-   *
-   * @param {Number} x
-   * @param {Number} y
-   */
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.width = GRAIN_SIZE;
-    this.height = GRAIN_SIZE;
+class SandParticle {
+  constructor(color) {
+    this.color = `hsl(${color}, 80%, 40%)`;
     this.speed = GRAIN_SIZE * 1;
-    this.color = color;
-    this.stopped = false;
   }
 
-  /**
-   * @param {Array<Sand>} sand
-   */
-  hasGrainBelow(sand) {
-    for (const grain of sand) {
-      if (
-        [this.x == grain.x, this.y + this.height == grain.y].every((v) => v)
-      ) {
-        return true;
-      }
-    }
-    return false;
-  }
-  /**
-   * @param {Array<Sand>} sand
-   */
-  hasGrainBelowLeft(sand) {
-    for (const grain of sand) {
-      if (
-        [this.x - this.width == grain.x, this.y + this.height == grain.y].every(
-          (v) => v
-        )
-      ) {
-        return true;
-      }
-    }
-    return false;
-  }
-  /**
-   * @param {Array<Sand>} sand
-   */
-  hasGrainBelowRight(sand) {
-    for (const grain of sand) {
-      if (
-        [this.x + this.width == grain.x, this.y + this.height == grain.y].every(
-          (v) => v
-        )
-      ) {
-        return true;
-      }
-    }
-    return false;
-  }
-  /**
-   * @param {Array<Sand>} sand
-   */
-  goDownLeft(sand) {
-    if (this.y >= HEIGHT - this.height) {
-      return (this.y = HEIGHT - this.height);
-    }
-
-    if (this.hasGrainBelowLeft(sand) || this.x <= 0) {
-      return false;
-    }
-    this.x -= this.width;
-    this.y += this.width;
-    return true;
-  }
-  /**
-   * @param {Array<Sand>} sand
-   */
-  goDownRight(sand) {
-    if (this.y >= HEIGHT - this.height) {
-      return (this.y = HEIGHT - this.height);
-    }
-    if (this.hasGrainBelowRight(sand) || this.x >= WIDTH - this.width) {
-      return false;
-    }
-    this.x += this.width;
-    this.y += this.width;
-    return true;
-  }
-  /**
-   * @param {Array<Sand>} sand
-   */
-  update(sand) {
-    if (this.y >= HEIGHT - this.height) {
-      this.stopped = true;
-      return (this.y = HEIGHT - this.height);
-    }
-    if (this.hasGrainBelow(sand)) {
-      let shouldStop = false;
-      const goLeftFirst = Math.random() > 0.5;
-      if (goLeftFirst) {
-        shouldStop = !(this.goDownLeft(sand) || this.goDownRight(sand));
-      } else {
-        shouldStop = !(this.goDownRight(sand) || this.goDownLeft(sand));
-      }
-
-      if (shouldStop) {
-        this.stopped = true;
-      }
-
-      return
-    }
-    this.y += this.speed;
-  }
-
-  draw() {
-    ctx.fillStyle = `hsl(${this.color}, 100%, 50%)`;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+  draw(x, y) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(x * GRAIN_SIZE, y * GRAIN_SIZE, GRAIN_SIZE, GRAIN_SIZE);
   }
 }
 
-new Sandbox();
+new SandBox();
